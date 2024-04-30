8119865ba5171ceffebbf2c14cb0a443be0e6964
robinlovelace <rob00x@gmail.com>
2024-04-30 15:42:10 +0100

    Add first function

---
+++ R/corenet.R
@@ -0,0 +1,14 @@
+#' Generate a core network from road network data
+#' 
+#' Description to go here...
+#' 
+#' @param net A road network object, of class sf
+#' @param tbc An object to be determined
+#' @return A core network object, of class sf
+#' @export
+#' @examples
+#' corenet()
+corenet = function(net, tbc) {
+    data.frame(x = 1, y = 2) |>
+      utils::head()
+}
