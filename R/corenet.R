b8070c725038d39094aaf11d85500e4669607946
robinlovelace <rob00x@gmail.com>
2024-04-30 16:26:37 +0100

    Add example dataset

--- R/corenet.R
+++ R/corenet.R
@@ -13,18 +13,28 @@ corenet = function(net, tbc) {
       utils::head()
 }
 
+#' Get the Edinburgh road network, within radius of 6 km of the center
+#' 
+#' @export
+#' @examples
+#' get_edinburgh_6km()
+get_edinburgh_6km = function() {
+    sf::read_sf("https://github.com/nptscot/corenet/releases/download/v0.0.1/open_roads_edinburgh_6km.gpkg")
+}
 
 
-#' Data from edinburgh's OSM network
+#' Data from OS Open Roads, Edinburgh, 3 km radius
 #'
 #'
 #' @docType data
 #' @keywords datasets
-#' @name osm_edinburgh_demo
+#' @name os_edinburgh_demo
 #' @format An sf data frame
 #' @examples 
 #' library(sf)
-#' names(osm_edinburgh_demo)
-#' head(osm_edinburgh_demo)
-#' plot(osm_edinburgh_demo)
+#' # TODO: fix this
+#' # data(os_edinburgh_demo)
+#' # names(os_edinburgh_demo)
+#' os_edinburgh = get_edinburgh_6km()
+#' head(os_edinburgh)
 NULL
