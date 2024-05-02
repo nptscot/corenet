8de4d1a9b8219ad2cd0714391a7775afbc2e2c4c
robinlovelace <rob00x@gmail.com>
2024-05-02 16:33:15 +0100

    Document dataset

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Data from edinburgh's network from OpenRoads
+#' @docType data
+#' @keywords datasets
+#' @name os_edinburgh_demo
+#' @format A data frame
+#' @examples
+#' head(os_edinburgh_demo)
+NULL
