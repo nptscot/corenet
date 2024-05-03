740669f4b78b0bca82192fa363b88b9dd0ed755d
wangzhao0217 <wangzhao0217@gmail.com>
2024-05-03 11:58:12 +0100

    add demo data

--- R/corenet.R
+++ R/corenet.R
@@ -1,114 +1,55 @@
-#' Generate a core network from road network data
-#'
-#' 
-#' This function returns OSM keys that are relevant for active travel
-#'
-
-std_pkgs = c(
-              "sf", 
-              "dplyr", 
-              "tidygraph", 
-              "igraph", 
-              "mapview", 
-              "tidyverse", 
-              "osmextract", 
-              "geos", 
-              "dbscan", 
-              "sfnetworks", 
-              "rsgeo",
-              "targets",
-              "zonebuilder"
-            )
-
-dev_pkgs = c(
-              "stplanr", 
-              "rsgeo"
-            )
-
 # Declare global variables to avoid 'no visible binding' notes
-utils::globalVariables(c("edge_paths", "combined_network_tile", "all_fastest_bicycle_go_dutch", 
+utils::globalVariables(c("edge_paths", "influence_network", "all_fastest_bicycle_go_dutch", 
                          "weight", "to_linegraph", "edges", "group", "mean_potential", "LAD23NM", 
                          "road_function",  "grid_id", "density", 
                          "max_value", "min_value", "arterialness", "road_score"))
 
 #' This function prepare the base netwrok for generating cohesive cycling network using NPT data.
 #'
-#' @param combined_network_tile NPT road network object, class sf.
+#' @param influence_network NPT road network object, class sf.
 #' @param crs Coordinate reference system to use, default is "EPSG:27700".
 #' @param parameters A list containing specific parameters like 'coherent_area'.
 #' @return A list of two elements: a cohesive network and zone data, both as sf objects.
 #' @export
 
 
-cohesive_network_prep = function(combined_network_tile, crs = "EPSG:27700", parameters) {
-  # Initialize empty lists to store results and zones for each area
-  cohesive_network_list = list()
-  cohesive_zone_list = list()
+cohesive_network_prep = function(base_network, influence_network, target_zone, crs = "EPSG:27700",  key_attribute = "road_function", attribute_values = c("A Road", "B Road", "Minor Road")) {
+    base_network = sf::st_transform(base_network, crs)    
+    influence_network = sf::st_transform(influence_network, crs)
+    target_zone = sf::st_transform(target_zone, crs)
 
-  # Check for specified areas in the 'coherent_area' parameter
-  if (!is.null(parameters$coherent_area) && length(parameters$coherent_area) > 0) {
+    # Intersect and transform NPT data with the calculated zones
+    NPT_zones = influence_network[sf::st_union(target_zone), , op = sf::st_intersects] |> 
+                sf::st_transform(crs)
 
-    # Process each specified area
-    for (area in parameters$coherent_area) {
-      area_filename = gsub(" ", "_", area)
-      print(paste("Preparing the network data for the", area))
-      combined_network_tile = sf::st_transform(combined_network_tile, crs)
+    OS_zones = base_network[sf::st_union(target_zone), , op = sf::st_intersects]
 
-      # Read spatial data and filter for the specific area with transformation and buffering
-      sg_intermediatezone_bdry_2011 = sf::st_read("inputdata/sg_intermediatezone_bdry_2011.gpkg") |> sf::st_transform(crs = crs)
-      las_scotland_2023 = sf::st_read("inputdata/las_scotland_2023.geojson") |>
-                           sf::st_transform(crs = crs) |>
-                           dplyr::filter(LAD23NM == area) |>
-                           sf::st_buffer(dist = 4000)
+    # Dynamically filter the data using provided column name and attributes
+    filtered_OS_zones = OS_zones |> 
+                        dplyr::filter(!!rlang::sym(key_attribute) %in% attribute_values ) |> 
+                        sf::st_transform(crs) |> 
+                        sf::st_zm()
 
-      # Calculate zones within the specified buffer area
-      zones = sg_intermediatezone_bdry_2011[sf::st_union(las_scotland_2023), , op = sf::st_within]
-      zones$density = zones$TotPop2011 / zones$StdAreaHa  
+    # Assign functions for data aggregation based on network attribute_values 
+    name_list = names(NPT_zones)
 
-      # Intersect and transform NPT data with the calculated zones
-      NPT_zones = combined_network_tile[sf::st_union(zones), , op = sf::st_intersects] |> 
-                   sf::st_transform(crs)
-
-      # Read and process road network data
-      open_roads_national = sf::read_sf("inputdata/oproad_gb.gpkg", layer = "road_link") |> 
-                   sf::st_transform(crs)
-
-      OS_zones = open_roads_national[sf::st_union(zones), , op = sf::st_intersects]
-
-      filtered_OS_zones = OS_zones |> 
-                            dplyr::filter(road_function == 'A Road' | road_function == 'B Road' | road_function == 'Minor Road') |>
-                            sf::st_transform(crs) |>
-                            sf::st_zm()
-
-      # Assign functions for data aggregation based on network attributes
-      name_list = names(NPT_zones)
-
-      funs = list()
-      for (name in name_list) {
-        if (name == "geometry") {
-          next  # Correctly skip the current iteration if the name is "geometry"
-        } else if (name %in% c("gradient", "quietness")) {
-          funs[[name]] = mean  # Assign mean function for specified fields
-        } else {
-          funs[[name]] = sum  # Assign sum function for all other fields
-        }
+    funs = list()
+    for (name in name_list) {
+      if (name == "geometry") {
+        next  # Correctly skip the current iteration if the name is "geometry"
+      } else if (name %in% c("gradient", "quietness")) {
+        funs[[name]] = mean  # Assign mean function for specified fields
+      } else {
+        funs[[name]] = sum  # Assign sum function for all other fields
       }
-
-      # Merge road networks with specified parameters
-      # OS_NPT_zones = stplanr::rnet_merge(OS_zones, NPT_zones, dist = 15, funs = funs, segment_length = 20,max_angle_diff = 10)
-      filtered_OS_NPT_zones = stplanr::rnet_merge(filtered_OS_zones, NPT_zones, dist = 15, funs = funs, segment_length = 20,max_angle_diff = 10)
-      # Store the processed data for the current area
-      cohesive_network_list[[area]] = filtered_OS_NPT_zones
-      cohesive_zone_list[[area]] = zones
-
-      print(paste("Finished preparing the network data for the", area))
     }
 
-    return(list(cohesive_zone = cohesive_zone_list, cohesive_network = cohesive_network_list))
+    # Merge road networks with specified parameters
+    filtered_OS_NPT_zones = stplanr::rnet_merge(filtered_OS_zones, NPT_zones, dist = 15, funs = funs, segment_length = 20,max_angle_diff = 10)
 
-  } else {
-    print("No coherent area specified, proceeding with default settings.")
-  }
+  print(paste("Finished preparing the network data"))
+  
+  return(filtered_OS_NPT_zones)
 }
 
 
@@ -117,8 +58,8 @@ cohesive_network_prep = function(combined_network_tile, crs = "EPSG:27700", para
 #' This function processes the provided network data to extract and analyze
 #' the most significant routes based on a percentile threshold, performs spatial operations,
 #' clusters data points, and calculates the largest network component without dangles.
-#' @param combined_network_tile The NPT network, class sf.
-#' @param network_tile Spatial object ob.
+#' @param influence_network The NPT network, class sf.
+#' @param network Spatial object ob.
 #' @param combined_grid_buffer Additional spatial data, currently unused in the road_function
 #' @param crs Coordinate reference system for transformation, default is "EPSG:27700".
 #' @param dist The distance threshold used in path calculations, default is 10.
@@ -126,14 +67,15 @@ cohesive_network_prep = function(combined_network_tile, crs = "EPSG:27700", para
 #' @export
 
 
-corenet = function(combined_network_tile, network_tile, combined_grid_buffer, crs = "EPSG:27700", dist = 10) {
+corenet = function(influence_network, cohesive_base_network, target_zone, key_attribute = "all_fastest_bicycle_go_dutch",  crs = "EPSG:27700", dist = 10, threshold = 1500, road_scores = list("A Road" = 1, "B Road" = 1, "Minor Road" = 10000000)) {
 
-  if ("all_fastest_bicycle_go_dutch" %in% names(network_tile)) {
+  if (key_attribute %in% names(cohesive_base_network)) {
+    paste0("Using ", key_attribute, " as indicator for the network")
+    influence_network = sf::st_transform(influence_network, crs)
+    influence_network_split = stplanr::line_segment(influence_network, segment_length = 20, use_rsgeo = TRUE)
 
-    combined_network_tile = sf::st_transform(combined_network_tile, crs)
-    combined_network_tile_split = stplanr::line_segment(combined_network_tile, segment_length = 20, use_rsgeo = TRUE)
-
-    filtered_roads = combined_network_tile_split[combined_network_tile_split$all_fastest_bicycle_go_dutch > 1500, ]
+    # Assuming threshold is already defined somewhere in your code
+    filtered_roads = influence_network_split[influence_network_split[[key_attribute]] > threshold, ]
 
     # Calculate centroids of network segments
     centroids = sf::st_centroid(filtered_roads)
@@ -145,11 +87,11 @@ corenet = function(combined_network_tile, network_tile, combined_grid_buffer, cr
     unique_centroids = centroids[!duplicated(centroids$cluster), ]    
 
   } else {
-    grid_sf = combined_grid_buffer
+    grid_sf = target_zone
 
     grid_sf$grid_id = seq_len(nrow(grid_sf))
 
-    split_network = sf::st_intersection(combined_network_tile, grid_sf)
+    split_network = sf::st_intersection(influence_network, grid_sf)
 
     select_by_density = function(density) {
       if (density < 10) {
@@ -210,7 +152,7 @@ corenet = function(combined_network_tile, network_tile, combined_grid_buffer, cr
   }
 
   # Prepare network and calculate paths
-  prepared_network = prepare_network(network_tile, transform_crs = crs,  A_Road = 1, B_Road = 1, Minor_Road = 100000000)
+  prepared_network = prepare_network(cohesive_base_network, key_attribute , road_scores, transform_crs = crs) 
 
 
   all_paths = purrr::map_dfr(
@@ -246,7 +188,7 @@ corenet = function(combined_network_tile, network_tile, combined_grid_buffer, cr
 coherent_network_group = function(CITY, ZONE) {
   # library(tidygraph)
   # Generate coherent network
-  rnet_coherent = corenet(combined_network_tile, network_tile = CITY, combined_grid_buffer = ZONE)
+  rnet_coherent = corenet(influence_network, network = CITY, combined_grid_buffer = ZONE)
   
   # Select relevant columns
   rnet_coherent_selected = rnet_coherent |>
@@ -285,7 +227,9 @@ coherent_network_group = function(CITY, ZONE) {
 #' road_scores = list("A Road" = 1, "B Road" = 1, "Minor Road" = 100000)
 #' @param transform_crs Numeric CRS code for coordinate transformation, default is 27700.
 #' @return An sfnetwork object with additional attributes like 'arterialness' and 'weight' based on road conditions.
-prepare_network = function(network, A_Road = 1, B_Road = 1, Minor_Road = 10000000, transform_crs = 27700) {
+prepare_network = function(network, key_attribute = "all_fastest_bicycle_go_dutch", 
+                           road_scores = list("A Road" = 1, "B Road" = 1, "Minor Road" = 10000000),
+                           transform_crs = 27700) {
     # Cast the network to LINESTRING and transform to the specified CRS
     network = network |>
         sf::st_cast("LINESTRING") |>
@@ -293,30 +237,29 @@ prepare_network = function(network, A_Road = 1, B_Road = 1, Minor_Road = 1000000
         sfnetworks::as_sfnetwork(directed = FALSE) |>
         sfnetworks::activate("edges") |>
         dplyr::mutate(
-            # Normalize 'all_fastest_bicycle_go_dutch' and handle NA values
-            all_fastest_bicycle_go_dutch = dplyr::if_else(is.na(all_fastest_bicycle_go_dutch), 0, all_fastest_bicycle_go_dutch),
-            min_value = min(all_fastest_bicycle_go_dutch, na.rm = TRUE),
-            max_value = max(all_fastest_bicycle_go_dutch, na.rm = TRUE),
+            # Handle NA values and normalize using key_attribute
+            value = dplyr::if_else(is.na(!!rlang::sym(key_attribute)), 0, !!rlang::sym(key_attribute)),
+            min_value = min(value, na.rm = TRUE),
+            max_value = max(value, na.rm = TRUE),
             arterialness = dplyr::if_else(
                 max_value > min_value,
-                (all_fastest_bicycle_go_dutch - min_value) / (max_value - min_value),
-                0  # Avoid division by zero by setting arterialness to 0 when min_value equals max_value
+                (value - min_value) / (max_value - min_value),
+                0  # Avoid division by zero
             ),
-            # Dynamically apply road type scores
-            road_score = dplyr::case_when(
-                road_function == "A Road" ~ A_Road,
-                road_function == "B Road" ~ B_Road,
-                road_function == "Minor Road" ~ Minor_Road,
-                TRUE ~ 0  # Handle possible NA or unexpected values
-            ),
+            # Dynamically apply road type scores from the road_scores list
+            road_score = purrr::map_dbl(road_function, function(x) {
+                if (x %in% names(road_scores)) {
+                    road_scores[[x]]
+                } else {
+                    0  # Default score for unrecognized road types
+                }
+            }),
             # Calculate weight considering the road type influence
             weight = (1 - arterialness) * 100 * (1 + 0.1 * road_score)
         )
 
     return(network)
 }
-
-
 #' Calculate paths from a given point to centroids within a specified distance range
 #'
 #' This function determines the network paths from a specific point to multiple centroids based on distance thresholds,
@@ -381,28 +324,28 @@ calculate_paths_from_point_dist = function(network, point, dist = 500, centroids
 #' This function takes a spatial network represented as an `sf` object, converts it into a graph format,
 #' identifies all connected components, and extracts the largest one.
 #'
-#' @param network_tile An `sf` object representing the network to be analyzed.
+#' @param network An `sf` object representing the network to be analyzed.
 #' @return An `sfnetwork` object representing the largest connected component of the network.
-calculate_largest_component = function(network_tile) {
+calculate_largest_component = function(network) {
     # Ensure the network tile is in the correct format and convert to sfnetwork
-    if (!inherits(network_tile, "sfnetwork")) {
-        network_tile_sfn = sfnetworks::as_sfnetwork(network_tile, directed = FALSE)
+    if (!inherits(network, "sfnetwork")) {
+        network_sfn = sfnetworks::as_sfnetwork(network, directed = FALSE)
     } else {
-        network_tile_sfn = network_tile
+        network_sfn = network
     }
 
     # Convert sfnetwork to tbl_graph for easier graph manipulations
-    network_tile_igraph = tidygraph::as_tbl_graph(network_tile_sfn)
+    network_igraph = tidygraph::as_tbl_graph(network_sfn)
 
     # Compute connected components using igraph
-    components = igraph::components(network_tile_igraph)
+    components = igraph::components(network_igraph)
 
     # Find the largest component by size
     largest_component_id = which.max(components$csize)
     largest_component_nodes = which(components$membership == largest_component_id)
 
     # Extract the subgraph corresponding to the largest component
-    largest_component_subgraph = igraph::subgraph(network_tile_igraph, largest_component_nodes)
+    largest_component_subgraph = igraph::subgraph(network_igraph, largest_component_nodes)
 
     # Convert back to sfnetwork
     largest_component_sfn = sfnetworks::as_sfnetwork(largest_component_subgraph, directed = FALSE)
@@ -416,17 +359,17 @@ calculate_largest_component = function(network_tile) {
 #' This function identifies and removes dangling line segments (lines with one end not connected to another line)
 #' within a specified tolerance.
 #'
-#' @param road_network An `sf` object of class LINESTRING representing the road network.
+#' @param network An `sf` object of class LINESTRING representing the road network.
 #' @param tolerance The distance tolerance for identifying isolated endpoints as dangling.
 #' @return An `sf` object with dangling line segments removed.
-removeDangles = function(road_network, tolerance = 0.001) {
+removeDangles = function(network, tolerance = 0.001) {
     # Convert to Spatial Lines if not already
-    road_network_lines = sf::st_cast(road_network, "LINESTRING")
+    network_lines = sf::st_cast(network, "LINESTRING")
 
     # Extract and combine all end points of line segments
-    end_points = do.call(rbind, lapply(road_network_lines$geometry, function(line) {
+    end_points = do.call(rbind, lapply(network_lines$geometry, function(line) {
     endpoints = rbind(sf::st_coordinates(line)[1, ], sf::st_coordinates(line)[nrow(sf::st_coordinates(line)), ])
-    sf::st_as_sf(data.frame(x = endpoints[, 1], y = endpoints[, 2]), coords = c("x", "y"), crs = sf::st_crs(road_network))
+    sf::st_as_sf(data.frame(x = endpoints[, 1], y = endpoints[, 2]), coords = c("x", "y"), crs = sf::st_crs(network))
     }))
 
     # Identify unique end points (potential dangles) using a spatial join to find nearby points
@@ -435,14 +378,14 @@ removeDangles = function(road_network, tolerance = 0.001) {
     isolated_points = end_points[rowSums(overlaps) == 1,]
 
     # Filter out road segments that end in these isolated points
-    segments_with_dangles = sapply(sf::st_geometry(road_network_lines), function(geom) {
-    ends = sf::st_sfc(sf::st_point(sf::st_coordinates(geom)[1,]), sf::st_point(sf::st_coordinates(geom)[nrow(sf::st_coordinates(geom)),]), crs = sf::st_crs(road_network))
+    segments_with_dangles = sapply(sf::st_geometry(network_lines), function(geom) {
+    ends = sf::st_sfc(sf::st_point(sf::st_coordinates(geom)[1,]), sf::st_point(sf::st_coordinates(geom)[nrow(sf::st_coordinates(geom)),]), crs = sf::st_crs(network))
     any(sf::st_intersects(ends, isolated_points, sparse = FALSE))
     })
 
-    road_network_without_dangles = road_network_lines[!segments_with_dangles, ]
+    network_without_dangles = network_lines[!segments_with_dangles, ]
 
-    return(road_network_without_dangles)
+    return(network_without_dangles)
 }
 
 
--- data-raw/osm_edinburgh_demo.R
+++ data-raw/osm_edinburgh_demo.R
@@ -1,8 +1,18 @@
 ## code to prepare `osm_edinburgh_demo` dataset goes here
 
+# Load necessary libraries
 library(tidyverse)
-remotes::install_github("nptscot/osmactive")
 library(osmactive)
+library(zonebuilder)
+library(sf)
+
+# Install required GitHub package if not already installed
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+remotes::install_github("nptscot/osmactive", quietly = TRUE)
+
+# Define the geographic zone for Edinburgh at different radii
 edinburgh = zonebuilder::zb_zone("Edinburgh")
 edinburgh_3km = edinburgh |>
   # Change number in next line to change zone size:
@@ -14,6 +24,8 @@ edinburgh_6km = edinburgh |>
   filter(circle_id <= 3) |>
   sf::st_union() |>
   sf::st_transform(27700)
+
+# Get Open Roads data
 if (!file.exists("srn.gpkg")) {
   message("Missing the SRN dataset locally, download it from the DfT or releases")
   u = "https://api.os.uk/downloads/v1/products/OpenRoads/downloads?area=GB&format=GeoPackage&redirect"
@@ -47,34 +59,67 @@ if (!file.exists("srn.gpkg")) {
       form_of_way = form_of_way,
       road_classification = road_classification
     )
-  open_roads_edinburgh = open_roads_cleaned[edinburgh_3km, , op = sf::st_within]
+  open_roads_edinburgh_3km = open_roads_national[edinburgh_3km, , op = sf::st_within] |>
+    dplyr::select(road_function)
   # Check the size of the dataset:
-  nrow(open_roads_edinburgh) / nrow(open_roads_national)
-  object.size(open_roads_edinburgh) |>
+  nrow(open_roads_edinburgh_3km) / nrow(open_roads_national)
+  object.size(open_roads_edinburgh_3km) |>
     # Format in MB:
     format(units = "MB")
-  sf::write_sf(open_roads_edinburgh, "open_roads_edinburgh.gpkg", delete_dsn = TRUE)
+  sf::write_sf(open_roads_edinburgh_3km, "open_roads_edinburgh_3km.gpkg", delete_dsn = TRUE)
+  
+  open_roads_edinburgh_6km = open_roads_national[edinburgh_6km, , op = sf::st_within] |>
+    dplyr::select(road_function)
+  # Check the size of the dataset:
+  nrow(open_roads_edinburgh_6km) / nrow(open_roads_national)
+  object.size(open_roads_edinburgh_6km) |>
+    # Format in MB:
+    format(units = "MB")
+  sf::write_sf(open_roads_edinburgh_6km, "open_roads_edinburgh_6km.gpkg", delete_dsn = TRUE)  
+  
   # Release data:
   if (FALSE) {
     usethis::use_github_release()
-    system("gh release upload v0.0.1 open_roads_edinburgh.gpkg --clobber")
+    system("gh release upload v0.0.1 open_roads_edinburgh_3km.gpkg --clobber")
+    system("gh release upload v0.0.1 open_roads_edinburgh_6km.gpkg --clobber")
   }
-  # Same for 6km:
-  open_roads_edinburgh_6km = open_roads_cleaned[edinburgh_6km, , op = sf::st_within]
-  sf::write_sf(open_roads_edinburgh_6km, "open_roads_edinburgh_6km.gpkg", delete_dsn = TRUE)
-    # Release data:
-    if (FALSE) {
-        system("gh release upload v0.0.1 open_roads_edinburgh_6km.gpkg --clobber")
-        }
 }
 
-os_edinburgh_demo = sf::read_sf("open_roads_edinburgh.gpkg")
-# Remove CRS info to avoid warning with non ascii characters:
-sf::st_crs(os_edinburgh_demo) = NA
-usethis::use_data(os_edinburgh_demo, overwrite = TRUE)
-# Let's see how big the file is:
-fs::file_size("data/os_edinburgh_demo.rda")
+os_edinburgh_demo_3km = sf::read_sf("open_roads_edinburgh_3km.gpkg")|>
+  sf::st_transform(27700)
+os_edinburgh_demo_6km = sf::read_sf("open_roads_edinburgh_6km.gpkg")|>
+  sf::st_transform(27700)
 
+# Remove CRS info to avoid warning with non ascii characters:
+sf::st_crs(os_edinburgh_demo_3km) = NA
+usethis::use_data(os_edinburgh_demo_3km, overwrite = TRUE)
+
+sf::st_crs(os_edinburgh_demo_6km) = NA
+usethis::use_data(os_edinburgh_demo_6km, overwrite = TRUE)
+# Let's see how big the file is:
+fs::file_size("data/os_edinburgh_demo_3km.rda")
+fs::file_size("data/os_edinburgh_demo_6km.rda")
+
+# Get npt data
+NPT = sf::st_read("https://github.com/nptscot/corenet/releases/download/testing_data/combined_network_tile.geojson") |>
+  sf::st_transform(27700)
+NPT_demo_3km = NPT[edinburgh_3km, , op = sf::st_within]|>
+    dplyr::select(all_fastest_bicycle_go_dutch)
+NPT_demo_6km = NPT[edinburgh_6km, , op = sf::st_within]|>
+    dplyr::select(all_fastest_bicycle_go_dutch)
+
+sf::write_sf(NPT_demo_3km, "NPT_demo_3km.gpkg", delete_dsn = TRUE)  
+sf::write_sf(NPT_demo_6km, "NPT_demo_6km.gpkg", delete_dsn = TRUE)  
+
+sf::st_crs(NPT_demo_3km) = NA
+usethis::use_data(NPT_demo_3km, overwrite = TRUE)
+
+sf::st_crs(NPT_demo_6km) = NA
+usethis::use_data(NPT_demo_6km, overwrite = TRUE)
+
+# Let's see how big the file is:
+fs::file_size("data/NPT_demo_3km.rda")
+fs::file_size("data/NPT_demo_6km.rda")
 
 # Get OSM data (for future reference)
 osm = get_travel_network("Scotland", boundary = edinburgh_3km, boundary_type = "clipsrc")
